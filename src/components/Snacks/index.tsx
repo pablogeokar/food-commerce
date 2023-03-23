0ea0eeb1753fa33f58fefe45fc0680ee321fd408
Pablo George <pablogeokar@hotmail.com>
2023-03-23 14:50:48 -0300

    adicioando o componente skeleton

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,5 +1,6 @@
 import { FiPlus } from 'react-icons/fi'
 import { currencyFormat } from '../../helpers/currencyFormat'
+import { SkeletonSnacks } from './SkeletonSnacks'
 import { Container } from './styles'
 
 interface SnacksProps {
@@ -8,19 +9,21 @@ interface SnacksProps {
 export default function Snacks({ snacks }: SnacksProps) {
   return (
     <Container>
-      {snacks.map((snack) => (
-        <div key={snack.id} className='snack'>
-          <h2>{snack.name}</h2>
-          <img src={snack.image} alt={snack.name} />
-          <p>{snack.description}</p>
-          <div>
-            <strong>{currencyFormat(snack.price)}</strong>
-            <button type='button'>
-              <FiPlus />
-            </button>
-          </div>
-        </div>
-      ))}
+      {!snacks.length
+        ? [1, 2, 3, 4].map((n) => <SkeletonSnacks key={n} />)
+        : snacks.map((snack) => (
+            <div key={snack.id} className='snack'>
+              <h2>{snack.name}</h2>
+              <img src={snack.image} alt={snack.name} />
+              <p>{snack.description}</p>
+              <div>
+                <strong>{currencyFormat(snack.price)}</strong>
+                <button type='button'>
+                  <FiPlus />
+                </button>
+              </div>
+            </div>
+          ))}
     </Container>
   )
 }
